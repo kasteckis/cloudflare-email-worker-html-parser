64d90e52be779c08ed84d79fe850f376ecf9ea98
Valentinas <plankton546@gmail.com>
2025-12-26 18:20:05 +0200

    Optimize code to make it more readable

--- src/index.js
+++ src/index.js
@@ -1,39 +1,21 @@
-import PostalMime from "postal-mime";
-
-async function streamToArrayBuffer(stream, streamSize) {
-    let result = new Uint8Array(streamSize);
-    let bytesRead = 0;
-    const reader = stream.getReader();
-    while (true) {
-        const { done, value } = await reader.read();
-        if (done) {
-            break;
-        }
-        result.set(value, bytesRead);
-        bytesRead += value.length;
-    }
-    return result;
-}
+import * as PostalMime from 'postal-mime';
 
 export default {
-    async email(event, env, ctx) {
-        const rawEmail = await streamToArrayBuffer(event.raw, event.rawSize);
-        const parser = new PostalMime();
-        const parsedEmail = await parser.parse(rawEmail);
-        console.log("Mail subject: ", parsedEmail.subject);
-        console.log("Mail message ID", parsedEmail.messageId);
-        console.log("HTML version of Email: ", parsedEmail.html);
-        console.log("Text version of Email: ", parsedEmail.text);
-        if (parsedEmail.attachments.length == 0) {
-            console.log("No attachments");
-        } else {
-            parsedEmail.attachments.forEach((att) => {
-                console.log("Attachment: ", att.filename);
-                console.log("Attachment disposition: ", att.disposition);
-                console.log("Attachment mime type: ", att.mimeType);
-                console.log("Attachment size: ", att.content.byteLength);
-            });
-        }
-        await event.forward("youremail@gmail.com");
+    async email(message, env, ctx) {
+        const parser = new PostalMime.default();
+        const rawEmail = new Response(message.raw);
+        const email = await parser.parse(await rawEmail.arrayBuffer());
+
+        console.log("message.from: ", message.from);
+        console.log("message.to: ", message.to);
+
+        console.log("Subject: ", email.subject);
+        console.log("From name: ", email.from.name);
+        console.log("From address: ", email.from.address);
+        console.log("To: ", email.to); // This is array (email.to[0].address)
+        console.log("HTML: ", email.html);
+        console.log(email);
+
+        // await message.forward("youremail@gmail.com");
     },
 };
